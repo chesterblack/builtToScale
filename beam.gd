3fb3fa38de99cad7bbbbd091b0baa11fe906162d
chesterblack <chez.black@hotmail.co.uk>
2024-08-17 13:42:11 +0100

    Reflection rotation is probably working

--- beam.gd
+++ beam.gd
@@ -1,25 +1,43 @@
 extends Node2D
 
-var is_active : bool = true
+@export var is_active : bool = true
+@export var ignored_colliders : Array[Area2D]
+
 var is_reflected : bool = false
 var raycast : RayCast2D
+var line : Line2D
 
 func _ready():
 	raycast = $RayCast2D
+	line = $Line2D
 
 
 func _process(delta):
 	visible = is_active
+	line.clear_points()
+	line.add_point(raycast.position)
+	line.add_point(raycast.target_position)
 
 
 func _physics_process(delta):
 	if is_active:
 		var collider = raycast.get_collider()
-		is_reflected = false
-		
-		if collider is Reflector:
-			is_reflected = true
-			collider.in_light.emit(self)
-		
-		if collider is Goal:
-			print("WIN")
+		if collider:
+			if collider not in ignored_colliders:
+				is_reflected = false
+				
+				if collider is Reflector:
+					var reflected_raycast = collider.reflected_beam.raycast as RayCast2D
+					
+					is_reflected = true
+					var laser_coll_point = raycast.get_collision_point()
+					var laser_coll_normal = raycast.get_collision_normal()
+					collider.reflected_beam.global_position = laser_coll_point
+					var forward = laser_coll_point - raycast.global_position
+					var reflection = forward.reflect(laser_coll_normal)
+					
+					reflected_raycast.target_position = -reflection
+					#collider.reflected_beam = reflection.angle()
+				
+				if collider is Goal:
+					print("WIN")
--- reflector.gd
+++ reflector.gd
@@ -2,24 +2,31 @@ class_name Reflector extends Area2D
 
 signal in_light
 
-var reflection_point : Node2D
+var reflected_beam : Node2D
 var reflecting_emitter : Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	reflection_point = $ReflectionPoint
+	reflected_beam = $ReflectedBeam
 	
 	in_light.connect(_on_in_light)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	reflection_point.is_active = false
+	reflected_beam.raycast.global_rotation = 0.0
+	#reflected_beam.is_active = false
 	if reflecting_emitter:
 		if reflecting_emitter.is_reflected:
-			reflection_point.is_active = true
+			reflected_beam.is_active = true
+			pass
 
 
-func _on_in_light(emitter):
-	reflecting_emitter = emitter
+func _on_in_light(emitter, normal, point):
+	pass
+	#get_node("/root/Node2D/Target").global_position = normal
+	#print(normal)
+	#reflecting_emitter = emitter
+	#reflected_beam.global_position = point
+	#reflected_beam.look_at(normal)
