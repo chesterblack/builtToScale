1b5db7a2e6f414448714462201f7e678eb35e99b
chesterblack <chez.black@hotmail.co.uk>
2024-08-17 14:56:05 +0100

    Beams create their own reflections rather than relying on the reflector to have a child beam already

--- beam.gd
+++ beam.gd
@@ -1,43 +1,63 @@
-extends Node2D
+class_name Beam extends Node2D
 
 @export var is_active : bool = true
-@export var ignored_colliders : Array[Area2D]
+@export var ignored_colliders : Array
+@export var length : float = 1000.0
 
 var is_reflected : bool = false
 var raycast : RayCast2D
 var line : Line2D
+var reflected_beam : Node2D
+
 
 func _ready():
 	raycast = $RayCast2D
 	line = $Line2D
 
-
 func _process(delta):
 	visible = is_active
-	line.clear_points()
-	line.add_point(raycast.position)
-	line.add_point(raycast.target_position)
+
+
+func test_function():
+	return "exists"
 
 
 func _physics_process(delta):
+	is_reflected = false
+	raycast.target_position = raycast.target_position.normalized() * length
+	
 	if is_active:
-		var collider = raycast.get_collider()
-		if collider:
-			if collider not in ignored_colliders:
-				is_reflected = false
+		line.clear_points()
+		line.add_point(raycast.position)
+		line.add_point(raycast.target_position)
+		
+		if raycast.is_colliding():
+			var collider = raycast.get_collider()
+			
+			if collider not in ignored_colliders and collider is Reflector:
+				if !reflected_beam:
+					reflected_beam = load("res://beam.tscn").instantiate()
+					add_child(reflected_beam)
+					reflected_beam.ignored_colliders = []
+					reflected_beam.ignored_colliders.append(collider)
 				
-				if collider is Reflector:
-					var reflected_raycast = collider.reflected_beam.raycast as RayCast2D
-					
-					is_reflected = true
-					var laser_coll_point = raycast.get_collision_point()
-					var laser_coll_normal = raycast.get_collision_normal()
-					collider.reflected_beam.global_position = laser_coll_point
-					var forward = laser_coll_point - raycast.global_position
-					var reflection = forward.reflect(laser_coll_normal)
-					
-					reflected_raycast.target_position = -reflection
-					#collider.reflected_beam = reflection.angle()
+				is_reflected = true
+				var collision_point = raycast.get_collision_point()
+				var collision_normal = raycast.get_collision_normal().normalized()
+				var forward = collision_point - raycast.global_position
+				var reflection = forward.reflect(collision_normal).normalized() * length
 				
-				if collider is Goal:
-					print("WIN")
+				print(self, ignored_colliders, collider)
+				reflected_beam.reflect_beam(collision_point, reflection)
+			
+			if collider is Goal:
+				print("WIN")
+	
+	if !is_reflected and reflected_beam:
+		reflected_beam.queue_free()
+		reflected_beam = null
+
+
+func reflect_beam(collision_point, reflection):
+	global_position = collision_point
+	raycast.target_position = -reflection
